d86f244466f5b62928921124458d69d0c1eff636
Cody Howarth <codyhowarth@gmail.com>
2020-04-04 12:16:13 -0400

    Update setup_venv.sh

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo apt install python3-venv
-python3 -m venv discord_notifications
-source discord_notifications/bin/activate
+python3 -m venv /home/cody/virtualenvironment/discord_scripts
+source /home/cody/virtualenvironment/discord_scripts/bin/activate
 pip install -r requirements.txt
 deactivate
 
@@ -10,5 +10,7 @@ deactivate
 
 read -p 'Please enter the discord webhook url so the script may post updates: ' webhookUrl
 
-sed -i s/\<webhook url goes here\>/$webhookUrl/g ./server_off_msg.py
-sed -i s/\<webhook url goes here\>/$webhookUrl/g ./server_on_msg.py
+sed -i "s!<webhook url goes here>!$webhookUrl!g" /home/cody/MinecraftBedrockServer/server_off_msg.py
+sed -i "s!<webhook url goes here>!$webhookUrl!g" /home/cody/MinecraftBedrockServer/server_on_msg.py
+cp /home/cody/MinecraftBedrockServer/server_off_msg.py /home/cody
+cp /home/cody/MinecraftBedrockServer/server_on_msg.py /home/cody
